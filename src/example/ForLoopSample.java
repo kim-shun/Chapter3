efeb2303f0d7dcfe5a85be6f2acb2a262f17acf3
kim-shun <kimura1417080@gmail.com>
2021-07-02 17:55:37 +0900

    繰り返し処理

---
+++ src/example/ForLoopSample.java
@@ -0,0 +1,12 @@
+package example;
+
+public class ForLoopSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for (int count = 0; count < 5; ++count) {
+			System.out.println("お腹がすいた！" + "[" + count + "回目]");
+		}
+	}
+
+}
