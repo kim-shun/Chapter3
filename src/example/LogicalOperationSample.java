bd69e9da9a67557d0febbbdc381e387df46469d4
kim-shun <kimura1417080@gmail.com>
2021-07-02 17:34:02 +0900

    論理演算子

---
+++ src/example/LogicalOperationSample.java
@@ -0,0 +1,13 @@
+package example;
+
+public class LogicalOperationSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age = 24;
+		double height = 168;
+		System.out.println(age > 20 && height > 170);
+		System.out.println(age > 20 || height > 170);
+	}
+
+}
