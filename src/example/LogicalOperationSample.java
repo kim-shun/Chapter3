0c2bdf5244fce20d84ced4869302ec3e36f6b201
kim-shun <kimura1417080@gmail.com>
2021-07-02 17:37:41 +0900

    条件分岐

--- src/example/LogicalOperationSample.java
+++ src/example/LogicalOperationSample.java
@@ -4,10 +4,16 @@ public class LogicalOperationSample {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		String name = "太郎";
 		int age = 24;
-		double height = 168;
+		double height = 173;
 		System.out.println(age > 20 && height > 170);
 		System.out.println(age > 20 || height > 170);
+		
+		if (height >= 168) {
+			System.out.println(name + "の身長は" + height + "cmです");
+		}
+		System.out.println(name + "は" + age + "歳です");
 	}
 
 }
