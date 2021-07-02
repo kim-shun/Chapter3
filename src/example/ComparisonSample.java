23f43cb53ee9cb9a2d116f2a9e5afa62c27ed3d6
kim-shun <kimura1417080@gmail.com>
2021-07-02 16:47:56 +0900

    Initial commit

---
+++ src/example/ComparisonSample.java
@@ -0,0 +1,16 @@
+package example;
+
+public class ComparisonSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int number = 5;
+		System.out.println(number == 10);
+		System.out.println(number != 10);
+		System.out.println(number > 10);
+		System.out.println(number < 10);
+		System.out.println(number >= 10);
+		System.out.println(number <= 10);
+	}
+
+}
